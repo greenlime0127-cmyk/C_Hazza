a3251d16d52dfa61b4d31efbfa4efc3e9c0e16b7
greenlime0127-cmyk <green.lime.0127@gmail.com>
2026-02-19 18:19:17 +0900

    Update 소스.cpp

---
+++ Project1/Project1/소스.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <Windows.h>
+
+enum Color
+{
+	Black,
+	DarkBlue,
+	DarkGreen,
+	DarkCyan,
+	DarkRed,
+	DarkMagenta,
+	DarkYellow,
+	Gray,
+	DarkGray,
+	Blue,
+	Green,
+	Cyan,
+	Red,
+	Magenta,
+	Yellow,
+	White,
+};
+
+void HideCursor();
+void SetTextColor(Color color);
+void SetPosition(int x, int y);
+
+
+
+int main()
+{
+
+	HideCursor();
+
+	while (true)
+	{
+		system("cls");
+
+		SetTextColor(White);
+		SetPosition(10, 10);
+		printf("Hello world");
+
+
+		Sleep(50);
+	}
+
+	return 0;
+}
+
+void SetPosition(int x, int y)
+{
+	COORD pos;
+	pos.X = x * 2;
+	pos.Y = y;
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void SetTextColor(Color color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void HideCursor()
+{
+	CONSOLE_CURSOR_INFO info;
+	info.bVisible = false;
+	info.dwSize = 1;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
